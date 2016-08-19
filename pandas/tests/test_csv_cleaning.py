bd33c3bf064da3a26c9453b71e6b506714047050
Junjay Tan <junjay@junjaytan.com>
2016-08-19 15:25:10 -0400

    added function to nullify field in a dataframe based on value in another field

--- pandas/tests/test_csv_cleaning.py
+++ pandas/tests/test_csv_cleaning.py
@@ -1,9 +1,9 @@
 import unittest
 import pandas as pd
 import numpy as np
-from pandas.util.testing import assert_frame_equal
+from pandas.util.testing import assert_frame_equal, assert_series_equal
 
-from utilities.csv_cleaning import nullify_non_alphanum
+from utilities.csv_cleaning import nullify_non_alphanum, nullify_fields_if_field_matches_pattern
 
 class TestCsvCleaning_NullifyNonAlphaNum(unittest.TestCase):
     def setUp(self):
@@ -31,3 +31,47 @@ class TestCsvCleaning_NullifyNonAlphaNum(unittest.TestCase):
                              'field3': ['1dfasdfa!', 1, 'a', '1 . 2', np.nan, np.nan]})
 
         assert_frame_equal(mydf, expected)
+
+class TestCsvCleaning_NullifyFieldsIfFieldMatchesPattern(unittest.TestCase):
+    def setUp(self):
+        self.mydf = pd.DataFrame({'field1': ['hello', 'hello1', 'dontstop', 'why', 'HELLO', '3'],
+                             'field2': ['y', 'y', np.nan, '. .', 'z', 'a'],
+                             'field3': ['1dfasdfa!', 1, 'a', '1 . 2', '.|', np.nan]})
+
+    def test_nullifies_two_different_fields(self):
+        mydf = self.mydf.copy(deep=True)
+        pattern = 'hello'
+
+        expected = pd.DataFrame({'field1': ['hello', 'hello1', 'dontstop', 'why', 'HELLO', '3'],
+                             'field2': [np.nan, np.nan, np.nan, '. .', np.nan, 'a'],
+                             'field3': [np.nan, np.nan, 'a', '1 . 2', np.nan, np.nan]})
+
+        nullify_fields_if_field_matches_pattern(mydf, 'field1', pattern, ['field2', 'field3'], case_sensitive=False)
+
+        assert_series_equal(expected['field1'], mydf['field1'])
+        assert_series_equal(expected['field2'], mydf['field2'])
+        assert_series_equal(expected['field3'], mydf['field3'])
+
+    def test_nullifies_same_field(self):
+        mydf = self.mydf.copy(deep=True)
+        pattern = 'hello'
+
+        expected = pd.DataFrame({'field1': [np.nan, np.nan, 'dontstop', 'why', np.nan, '3'],
+                             'field2': ['y', 'y', np.nan, '. .', 'z', 'a'],
+                             'field3': ['1dfasdfa!', 1, 'a', '1 . 2', '.|', np.nan]})
+
+        nullify_fields_if_field_matches_pattern(mydf, 'field1', pattern, ['field1'], case_sensitive=False)
+
+        assert_series_equal(expected['field1'], mydf['field1'])
+        assert_series_equal(expected['field2'], mydf['field2'])
+        assert_series_equal(expected['field3'], mydf['field3'])
+
+    def test_doesnt_nullify_anything(self):
+        mydf = self.mydf.copy(deep=True)
+        pattern = 'hello there'
+
+        expected = pd.DataFrame({'field1': ['hello', 'hello1', 'dontstop', 'why', 'HELLO', '3']})
+
+        nullify_fields_if_field_matches_pattern(mydf, 'field1', pattern, ['field1'], case_sensitive=False)
+
+        assert_series_equal(expected['field1'], mydf['field1'])
--- pandas/utilities/csv_cleaning.py
+++ pandas/utilities/csv_cleaning.py
@@ -17,6 +17,32 @@ def nullify_non_alphanum(dataframe, fields=None):
         for field in fields:
             dataframe[field].replace({r'^[^a-zA-Z0-9]*$': np.nan}, regex=True, inplace=True)
 
+def nullify_fields_if_field_matches_pattern(dataframe, field_containing_pattern, pattern, fields_to_nullify, case_sensitive=False):
+    """
+    Can be used to nullify one or more fields if another field matches a certain pattern.
+    Can also be used to nullify values in one field if values in that field match a certain pattern (i.e., blacklisting values)
+    Note: Keys must be unique by row; otherwise will not work correctly
+    :param dataframe:
+    :param field_containing_pattern:
+    :param fields_to_nullify: list of fieldnames specifying the fields to nullify. Can nullify the same field (i.e., blacklist)
+    :param case_sensitive: whether matching is case sensitive or not. Default is False (i.e., not case sensitive)
+    :return: None (nullifies the dataframe in place)
+    """
+    rows_that_contain = dataframe[field_containing_pattern].str.contains(pattern, case=case_sensitive)
+
+    if not rows_that_contain.index.is_unique:
+        raise IndexError("indexes in dataframe must be unique")
+
+    # store indexes of rows that match pattern in a list to be used for locating elements to update
+    # TODO: there is likely a more pandas-efficient way to do this (via slices, etc) instead of O(n) iteration
+    indexes_for_match = []
+    for idx, row in rows_that_contain.iteritems():
+        if row == True:     # because of numpy quirk, must use == instead of "is True" for boolean
+            indexes_for_match.append(idx)
+
+    for field in fields_to_nullify:
+        dataframe.loc[indexes_for_match, (field)] = np.nan
+
 
 def blacklist_values():
     raise NotImplemented()
