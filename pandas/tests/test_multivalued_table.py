c710ad17033675871083bee7560538cfd1fc8c74
Junjay Tan <junjay@junjaytan.com>
2016-08-19 15:37:47 -0400

    added utility class to widen any multivalued attribute tables by key into wide dataframe where each value is in separate column. Moved column uniques profiling method into this class

---
+++ pandas/tests/test_multivalued_table.py
@@ -0,0 +1,151 @@
+import unittest
+import pandas as pd
+import numpy as np
+from pandas.util.testing import assert_frame_equal, assert_series_equal
+
+from utilities.multivalued_table import MultivaluedTable
+
+class TestMultivaluedTableProfiling(unittest.TestCase):
+    # need to test on dataframes with numbers in values
+    # need to add case insensitive option
+    def setUp(self):
+        self.mydf = pd.DataFrame({'key': ['1', '1', '2', '2', '2', '3'],
+                      'field1': ['y', 'y', np.nan, 'x', 'z', 'a'],
+                      'field2': ['y', 'z', 'a', 'a', 'a', np.nan]})
+
+    def test_profiling_works_for_strings_and_NaNs(self):
+        mydf = self.mydf.copy(deep=True)
+
+        expected = {'field1': {'min_uniques': 1, 'max_uniques': 2, 'avg_uniques': 1.3333333333333333},
+                    'field2': {'min_uniques': 0, 'max_uniques': 2, 'avg_uniques': 1.0}}
+
+        result = MultivaluedTable.profile_uniques(mydf, 'key', ['field1', 'field2'], outfile=None)
+
+        self.assertEqual(result, expected)
+
+    @unittest.skip("Need to implement testing output file")
+    def test_write_to_output_file(self):
+        mydf = self.mydf.copy(deep=True)
+
+        result = MultivaluedTable.profile_uniquesprofile_uniques(mydf, 'key', ['field1', 'field2'], outfile='testfile.csv')
+
+class TestMultivaluedTableWidening(unittest.TestCase):
+    def setUp(self):
+        self.mydf = pd.DataFrame({'key': ['aa', 'aa', 'bb', 'bb', 'bb', 'bb', 'cc', 'dd', 'ee'],
+                      'field1': ['y', 'y', np.nan, 'x', 'y', 'xy', 'z', 'a', np.nan],
+                      'field2': ['y', 'z', 'a', 'a', 'a', 'cc', np.nan, np.nan, np.nan],
+                      'unique_vals': ['1', '1', '2', '2', '2', '2', '3', '4', np.nan]})
+
+        self.uniques_profile_stats = MultivaluedTable.profile_uniques(self.mydf, 'key',
+                                                                        ['field1', 'field2', 'unique_vals'],
+                                                                        outfile=None)
+
+    def test_generate_unique_key_dataframe(self):
+        mydf = self.mydf.copy(deep=True)
+
+        result = MultivaluedTable._generate_unique_key_dataframe(mydf, 'key')
+
+        expected = pd.DataFrame([{'key': 'aa'},
+                                 {'key': 'bb'},
+                                 {'key': 'cc'},
+                                 {'key': 'dd'},
+                                 {'key': 'ee'}]).set_index('key')
+
+        assert_frame_equal(result, expected)
+
+
+    def test_unique_valued_field_works(self):
+        """ ensure that a unique valued field returns a deduplicated dataframe. Fields containing only NaNs will not appear
+         in the resulting dataframe """
+        mydf = self.mydf.copy(deep=True)
+
+        result = MultivaluedTable._unique_valued_field(mydf, 'key', 'unique_vals')
+
+        expected = pd.DataFrame([{'key': 'aa', 'unique_vals': '1'},
+                                 {'key': 'bb', 'unique_vals': '2'},
+                                 {'key': 'cc', 'unique_vals': '3'},
+                                 {'key': 'dd', 'unique_vals': '4'}]).set_index('key')
+
+        assert_frame_equal(result, expected)
+
+    def test_unique_valued_field_works_field_with_value_and_nan(self):
+        """ Logic used in counting the max number of uniques by key will say a field has only 1 max unique if possible value
+        for each key is a value and one or more NaNs. However, running pandas drop_duplicates will still retain these
+        as 2 possible rows, so we drop all NaNs before running drop duplicates. Verify that this is the behavior """
+        mydf = pd.DataFrame({'key': ['aa', 'aa', 'bb', 'bb', 'bb', 'bb', 'cc', 'dd', 'ee', 'ee'],
+                             'field1': ['y', 'y', np.nan, 'x', 'y', 'xy', 'z', 'a', np.nan, np.nan],
+                             'field2': ['y', 'z', 'a', 'a', 'a', 'cc', np.nan, np.nan, np.nan, np.nan],
+                             'unique_vals': ['1', '1', '2', '2', '2', '2', '3', '4', np.nan, 'myvalue']})
+
+        result = MultivaluedTable._unique_valued_field(mydf, 'key', 'unique_vals')
+
+        expected = pd.DataFrame([{'key': 'aa', 'unique_vals': '1'},
+                                 {'key': 'bb', 'unique_vals': '2'},
+                                 {'key': 'cc', 'unique_vals': '3'},
+                                 {'key': 'dd', 'unique_vals': '4'},
+                                 {'key': 'ee', 'unique_vals': 'myvalue'}]).set_index('key')
+        assert_frame_equal(result, expected)
+
+
+    def test_widen_one_field_default_limits(self):
+        """ ensure that a dataframe for a field with multivalued attributes returns the correct multi-column output,
+        with and without limits """
+        mydf = self.mydf.copy(deep=True)
+
+        result = MultivaluedTable._widen_one_field(mydf, 'key', 'field1', self.uniques_profile_stats)
+
+        expected = pd.DataFrame([{'key': 'aa', 'field1': 'y', 'field1_1': np.nan, 'field1_2': np.nan},
+                                 {'key': 'bb', 'field1': 'x', 'field1_1': 'y', 'field1_2': 'xy'},
+                                 {'key': 'cc', 'field1': 'z', 'field1_1': np.nan, 'field1_2': np.nan},
+                                 {'key': 'dd', 'field1': 'a', 'field1_1': np.nan, 'field1_2': np.nan},
+                                 {'key': 'ee', 'field1': np.nan, 'field1_1': np.nan, 'field1_2': np.nan}]).set_index('key')
+
+        assert_frame_equal(result, expected)
+
+
+    def test_widen_one_field_default_limit_2cols(self):
+        """
+        sample df has max of 3 columns, but you can limit it to output less than the max number of values per attribute
+        in the dataset. This tests when you set limit to only have 2 columns instead of 3
+        :return:
+        """
+        mydf = self.mydf.copy(deep=True)
+
+        result = MultivaluedTable._widen_one_field(mydf, 'key', 'field1', self.uniques_profile_stats, limit=2)
+
+        expected = pd.DataFrame([{'key': 'aa', 'field1': 'y', 'field1_1': np.nan},
+                                 {'key': 'bb', 'field1': 'x', 'field1_1': 'y'},
+                                 {'key': 'cc', 'field1': 'z', 'field1_1': np.nan},
+                                 {'key': 'dd', 'field1': 'a', 'field1_1': np.nan},
+                                 {'key': 'ee', 'field1': np.nan, 'field1_1': np.nan}]).set_index('key')
+
+        assert_frame_equal(result, expected)
+
+    def test_widen_multivalues_into_additional_columns(self):
+        mydf = self.mydf.copy(deep=True)
+
+        result = MultivaluedTable.widen_multivalues_into_additional_columns(mydf, 'key', ['field1', 'field2', 'unique_vals'])
+
+        expected = pd.DataFrame([{'key': 'aa', 'field1': 'y', 'field1_1': np.nan, 'field1_2': np.nan, 'field2': 'y', 'field2_1': 'z', 'unique_vals': '1'},
+                                 {'key': 'bb', 'field1': 'x', 'field1_1': 'y', 'field1_2': 'xy', 'field2': 'a', 'field2_1': 'cc', 'unique_vals': '2'},
+                                 {'key': 'cc', 'field1': 'z', 'field1_1': np.nan, 'field1_2': np.nan, 'field2': np.nan, 'field2_1': np.nan, 'unique_vals': '3'},
+                                 {'key': 'dd', 'field1': 'a', 'field1_1': np.nan, 'field1_2': np.nan, 'field2': np.nan, 'field2_1': np.nan, 'unique_vals': '4'},
+                                 {'key': 'ee', 'field1': np.nan, 'field1_1': np.nan, 'field1_2': np.nan, 'field2': np.nan, 'field2_1': np.nan, 'unique_vals': np.nan}]).set_index('key')
+
+        assert_frame_equal(result, expected)
+
+    def test_widen_multivalues_into_additional_columns_for_empty_column(self):
+        mydf = self.mydf.copy(deep=True)
+        mydf['empty_field'] = np.nan
+
+        result = MultivaluedTable.widen_multivalues_into_additional_columns(mydf, 'key',
+                                                                            ['field1', 'field2', 'unique_vals', 'empty_field'])
+
+        expected = pd.DataFrame([{'key': 'aa', 'field1': 'y', 'field1_1': np.nan, 'field1_2': np.nan, 'field2': 'y', 'field2_1': 'z', 'unique_vals': '1', 'empty_field': np.nan},
+                                 {'key': 'bb', 'field1': 'x', 'field1_1': 'y', 'field1_2': 'xy', 'field2': 'a', 'field2_1': 'cc', 'unique_vals': '2', 'empty_field': np.nan},
+                                 {'key': 'cc', 'field1': 'z', 'field1_1': np.nan, 'field1_2': np.nan, 'field2': np.nan, 'field2_1': np.nan, 'unique_vals': '3', 'empty_field': np.nan},
+                                 {'key': 'dd', 'field1': 'a', 'field1_1': np.nan, 'field1_2': np.nan, 'field2': np.nan, 'field2_1': np.nan, 'unique_vals': '4', 'empty_field': np.nan},
+                                 {'key': 'ee', 'field1': np.nan, 'field1_1': np.nan, 'field1_2': np.nan, 'field2': np.nan, 'field2_1': np.nan, 'unique_vals': np.nan, 'empty_field': np.nan},]).set_index('key')
+
+        assert_series_equal(result['field1'], expected['field1'])
+        assert_series_equal(result['empty_field'], expected['empty_field'])
---
+++ pandas/utilities/multivalued_table.py
@@ -0,0 +1,154 @@
+import pandas as pd
+import numpy as np
+import csv
+
+class MultivaluedTable(object):
+    """
+    This is a static method class. These MultivaluedTable methods are useful when rows in the original DataFrame may
+    not have unique keys within a specified key field, and you need to profile and restructure the DataFrame to contain
+    unique keys per row, transforming any multivalued attributes as necessary. You may have such a dataset
+    when someone gives you a denormalized csv created by joining several database tables, for example.
+    """
+    def __init__(self):
+        """
+        """
+
+    @staticmethod
+    def _generate_unique_key_dataframe(orig_dataframe, keyfield):
+        """ generates a new dataframe containing only unique keys. This will be the base dataframe for
+        merging additional columns containing widened attributes """
+
+        df_by_unique_key = pd.DataFrame()
+        df_by_unique_key[keyfield] = orig_dataframe[keyfield].unique()
+
+        return df_by_unique_key.set_index(keyfield)
+
+    @staticmethod
+    def profile_uniques(dataframe, keyfield, fields_to_profile, casesensitive=False, outfile=None):
+        """
+        Analyzes the max, min, and average number of unique values in a csv around a key field, grouped by each field
+        specified in the input. (NaNs are not counted as values). Outputs these results to a csv if desired.
+
+        :param keyfield: string representing the column name of key column
+        :param fields_to_profile: list of fieldnames identifying fields to provile
+        :param casesensitive: currently does not do anything
+        :param outfile: path and name of file to write results to
+        :return: a nested dictionary {"fieldname1": {"max_uniques": 1, ... }, "fieldname2": {...} }
+        """
+
+        # initializing dictwriter headers and list to hold dict objects
+        header = ["field", "max_uniques", "avg_uniques", "min_uniques"]
+        all_field_stats = dict()
+
+        for field in fields_to_profile:
+            # Equivalent to: SELECT keyfield, COUNT(distinct field) FROM table GROUP BY keyfield
+            count_by_key = dataframe.groupby(keyfield)[field].nunique()
+            fieldstats = {"max_uniques": count_by_key.max(),
+                          "avg_uniques": count_by_key.mean(),
+                          "min_uniques": count_by_key.min()}
+            all_field_stats[field] = fieldstats
+
+            del fieldstats, count_by_key
+
+        if outfile is not None:
+            # write results to an output file, overwriting any existing data
+            print("writing to outfile at location: " + str(outfile))
+            with open(outfile, 'w') as csvfile:
+                writer = csv.DictWriter(csvfile, fieldnames=header)
+                writer.writeheader()
+                for key in all_field_stats:
+                    writer.writerow({"field": key,
+                                     "max_uniques": all_field_stats[key]["max_uniques"],
+                                     "avg_uniques": all_field_stats[key]["avg_uniques"],
+                                     "min_uniques": all_field_stats[key]["min_uniques"]})
+
+        return all_field_stats
+
+    @staticmethod
+    def widen_multivalues_into_additional_columns(orig_dataframe, keyfield, fields, limit=10):
+        """ Profiles and then expands columns up to the specified limit, appending additional columns to the
+        original dataset as necessary to accommodate multi-valued attributes
+
+        :param fields: list of fieldnames to be widened
+        """
+
+        # profile the fields to determine max uniques by key. If max is 1 then is trivial
+        uniques_profile_stats = MultivaluedTable.profile_uniques(orig_dataframe, keyfield, fields)
+
+        # initialize dataframe that holds only unique entity ids, which will be merged to create final dataframe
+        entity_ids = MultivaluedTable._generate_unique_key_dataframe(orig_dataframe, keyfield)
+
+        merged_dataframe = entity_ids.copy(deep=True)
+
+        for field in fields:
+            # trivial case of there being only one unique value or no unique values per key
+            if uniques_profile_stats[field]['max_uniques'] <= 1:
+                df_to_join = MultivaluedTable._unique_valued_field(orig_dataframe, keyfield, field)
+            else:
+                df_to_join = MultivaluedTable._widen_one_field(orig_dataframe, keyfield, field, uniques_profile_stats, limit)
+
+            # now merge (join) to uniques table
+            merged_dataframe = merged_dataframe.merge(df_to_join, left_index=True, right_index=True, how='left')
+
+        return merged_dataframe
+
+    @staticmethod
+    def _unique_valued_field(orig_dataframe, keyfield, field):
+        """
+        returns a 2 column dataframe of keyfield (as index) and the field to be used. Works if field is empty, defined
+         as there only being NaNs for all values
+        :param field:
+        :return: a two-column dataframe consisting of deduplicated keys and values, with keys set as dataframe index
+        """
+        new_df = orig_dataframe[[keyfield, field]].dropna().drop_duplicates(subset=[keyfield, field]).set_index(keyfield)
+        return new_df
+
+    @staticmethod
+    def _widen_one_field(orig_dataframe, keyfield, field, uniques_profile_stats, limit=10):
+        """
+        Not very efficient, but does the job for now, especially since pandas pivot has bugs
+        :param dataframe:
+        :param keyfield:
+        :param field:
+        :param limit:
+        :return: dataframe with key field values as indexes and columns expanded
+        """
+
+        max_uniques = uniques_profile_stats[field]['max_uniques']
+
+        uniques_by_key = orig_dataframe.groupby(keyfield)[field].unique()
+        num_uniques_by_key = orig_dataframe.groupby(keyfield)[field].nunique()
+
+        # create a series for each possible value up to the max num of uniques or the limit. These will represent
+        # the additional columns used to hold multi-values
+        max_columns = min(max_uniques, limit)
+        values_by_col = {}  # initialize an empty list that will hold dictionaries which will be converted to series
+        for i in range(0, max_columns):
+            if i == 0:
+                fieldname = field
+            else:
+                fieldname = field + "_" + str(i)
+
+            values_by_col[fieldname] = {}  # initialize empty dicts, each dict will have unique series keys as its keys
+
+        for idx, val in uniques_by_key.iteritems():
+            # remove NaNs from consideration within each array of unique values when populating columns
+            # otherwise you can get NaNs populated first instead of actual values
+            # numpy arrays don't do well with object data types, so hacky conversion to list of strings for now
+            unique_values= [x for x in val.astype('str').tolist() if x != 'nan']
+
+            for i in range(0, max_columns):
+                if i == 0:
+                    fieldname = field
+                else:
+                    fieldname = field + "_" + str(i)
+
+                if i < num_uniques_by_key[idx]:
+                    values_by_col[fieldname][idx] = unique_values[i]
+                else:
+                    values_by_col[fieldname][idx] = np.nan
+
+        new_df = pd.DataFrame(values_by_col)
+        new_df.index.name = keyfield    # set index name
+
+        return new_df
