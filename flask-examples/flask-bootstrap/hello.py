b4b2d745fd5f45e32b31310409cd86e1630d490e
Junjay Tan <junjay@junjaytan.com>
2017-01-09 16:52:40 -0500

    main page is a submission form for user name

--- flask-examples/flask-bootstrap/hello.py
+++ flask-examples/flask-bootstrap/hello.py
@@ -1,12 +1,25 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'test'
 bootstrap = Bootstrap(app)
 
-@app.route('/')
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return '<h1>Hello world!</h1>'
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
 
 @app.route('/user/<name>')
 def user(name):
