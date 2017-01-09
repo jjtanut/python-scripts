9436feaaa1e77a059d2d95a77e8909ffc98a6194
Junjay Tan <junjay@junjaytan.com>
2017-01-09 16:16:18 -0500

    added templates and favicon

--- flask-examples/flask-bootstrap/hello.py
+++ flask-examples/flask-bootstrap/hello.py
@@ -13,5 +13,13 @@ def user(name):
     # dynamically generated route
     return render_template('user.html', name=name)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 if __name__ == '__main__':
     app.run(debug=True) # ensures that dev server is started only when script is executed directly
